67fb6153c37b0cd025c9ebe4634fb6cd6f0ece3e
Lucca Shizuru <luccashizuru@gmail.com>
2025-08-28 15:08:10 -0300

    iniciando projeto

---
+++ src/Domain/Entities/FoodRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace NutriTrack.src.Domain.Entities
+{
+    public class FoodRequest
+    {
+        public string Name { get; set; }
+        public decimal QuantityInGrams { get; set; }
+    }
+}
---
+++ src/Domain/Entities/MealRequest.cs
@@ -0,0 +1,14 @@
+﻿using NutriTrack.src.Domain.Enums;
+
+
+namespace NutriTrack.src.Domain.Entities
+{
+    public class MealRequest
+    {
+        public Guid? UserId { get; set; }
+        public DateTime Date { get; set; }
+        public MealType Type { get; set; }
+
+        public List<FoodRequest> Food { get; set; }
+    }
+}
