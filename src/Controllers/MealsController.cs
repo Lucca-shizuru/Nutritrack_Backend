93239dbb856645310dfc8a40bfe97601b6d9b8af
Lucca Shizuru <luccashizuru@gmail.com>
2026-02-11 02:48:00 -0300

    Refactor/ Adicionando os endpoints de update e delete

--- src/Controllers/MealsController.cs
+++ src/Controllers/MealsController.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using NutriTrack.src.Application.Features.Meals.Commands.DeleteMeal;
+using NutriTrack.src.Application.Features.Meals.Commands.UpdateMeal;
 using NutriTrack.src.Application.Features.Meals.Queries.DailySummary;
 using NutriTrack.src.Application.Features.Users.Commands.CreateMeal;
 using NutriTrack.src.Infraestructure.ExternalServices.Dtos;
@@ -36,5 +38,39 @@ namespace NutriTrack.src.Controllers
 
             return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMeal(Guid id, [FromBody] UpdateMealCommand command)
+        {
+            if (id != command.MealId)
+                return BadRequest("ID da URL difere do ID do corpo.");
+
+            
+            var userIdClaim = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            if (userIdClaim == null) return Unauthorized();
+
+            command.UserId = Guid.Parse(userIdClaim);
+
+            var result = await _mediator.Send(command);
+            if (!result.IsSuccess) return BadRequest(result.Error);
+
+            return Ok(result.Value);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMeal(Guid id)
+        {
+            var userIdClaim = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            if (userIdClaim == null) return Unauthorized();
+
+            var command = new DeleteMealCommand(id, Guid.Parse(userIdClaim));
+
+            var result = await _mediator.Send(command);
+            if (!result.IsSuccess) return BadRequest(result.Error);
+
+            return NoContent(); 
+        }
+
+
     }
 }
